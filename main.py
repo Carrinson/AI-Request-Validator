aa846312f9ccb40a6e9ab06c5856f80790a8ebb6
Carrinson <folacarrinson@gmail.com>
2026-07-12 12:52:03 +0100

    router registered in main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+from routers.validation import router as validation_router
 
 app = FastAPI()
+
+app.include_router(validation_router)
+
+@app.get("/")
+async def home():
+    return {"message": "Welcome to API Validator"}
